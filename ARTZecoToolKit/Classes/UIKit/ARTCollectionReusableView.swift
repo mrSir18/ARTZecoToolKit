a2f60c8e2c352ea0b78777a637fe708d65d78998
mrSir18 <mrSir18@163.com>
2023-11-27 01:26:58 +0800

    open类

--- ARTZecoToolKit/Classes/UIKit/ARTCollectionReusableView.swift
+++ ARTZecoToolKit/Classes/UIKit/ARTCollectionReusableView.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 // MARK: - ARTCollectionReusableView
-public class ARTCollectionReusableView: UICollectionReusableView {
+open class ARTCollectionReusableView: UICollectionReusableView {
 
-    public class func dequeueReusableCell(from collectionView: UICollectionView, ofKind elementKind: String, withReuseIdentifier identifier: AnyClass, for indexPath: IndexPath) -> ARTCollectionReusableView {
+    open class func dequeueReusableCell(from collectionView: UICollectionView, ofKind elementKind: String, withReuseIdentifier identifier: AnyClass, for indexPath: IndexPath) -> ARTCollectionReusableView {
         let reusableView = collectionView.dequeueReusableSupplementaryView(ofKind: elementKind, withReuseIdentifier: String(describing: identifier), for: indexPath) as! ARTCollectionReusableView
         return reusableView
     }
@@ -20,23 +20,23 @@ public class ARTCollectionReusableView: UICollectionReusableView {
         backgroundColor = UIColor.white
     }
 
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 }
 
 // MARK: - ARTElementKindSectionHeader
-public class ARTElementKindSectionHeader: ARTCollectionReusableView {
+open class ARTElementKindSectionHeader: ARTCollectionReusableView {
 
-    public class func dequeueHeader(from collectionView: UICollectionView, for indexPath: IndexPath) -> ARTCollectionReusableView {
+    open class func dequeueHeader(from collectionView: UICollectionView, for indexPath: IndexPath) -> ARTCollectionReusableView {
         return dequeueReusableCell(from: collectionView, ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: self, for: indexPath) as! ARTElementKindSectionHeader
     }
 }
 
 // MARK: - ARTElementKindSectionFooter
-public class ARTElementKindSectionFooter: ARTCollectionReusableView {
+open class ARTElementKindSectionFooter: ARTCollectionReusableView {
     
-    public class func dequeueFooter(from collectionView: UICollectionView, for indexPath: IndexPath) -> ARTCollectionReusableView {
+    open class func dequeueFooter(from collectionView: UICollectionView, for indexPath: IndexPath) -> ARTCollectionReusableView {
         return dequeueReusableCell(from: collectionView, ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: self, for: indexPath) as! ARTElementKindSectionFooter
     }
 }
