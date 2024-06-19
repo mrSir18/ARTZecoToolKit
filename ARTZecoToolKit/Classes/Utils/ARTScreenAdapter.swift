249fcf96cbabece198a277728f11d38ad2e4a93a
mrSir18 <>
2024-06-20 05:59:58 +0800

    比例适配

---
+++ ARTZecoToolKit/Classes/Utils/ARTScreenAdapter.swift
@@ -0,0 +1,25 @@
+//
+//  ARTScreenAdapter.swift
+//  ARTZecoToolKit
+//
+//  Created by mrSir18 on 2024/6/20.
+//
+
+import UIKit
+
+/// 获取适配比例
+public func ARTAdaptScaleFactor() -> CGFloat {
+    let screenWidth = UIScreen.main.bounds.size.width
+    let designWidth: CGFloat = 375.0
+    return screenWidth / designWidth
+}
+
+/// 适配值
+public func ARTAdaptedValue(_ value: CGFloat) -> CGFloat {
+    return value * ARTAdaptScaleFactor()
+}
+
+/// 适配尺寸
+public func ARTAdaptedSize(width: CGFloat, height: CGFloat) -> CGSize {
+    return CGSize(width: ARTAdaptedValue(width), height: ARTAdaptedValue(height))
+}
