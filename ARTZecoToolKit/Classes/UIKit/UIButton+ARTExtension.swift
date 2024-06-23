87fc707304d901a88625d5df6e973e1f2e9e3721
mrSir18 <>
2024-06-23 23:47:13 +0800

    自定义值，用于保存额外的数据.,Note: 使用 `objc_setAssociatedObject` 和 `objc_getAssociatedObject` 函数来实现

--- ARTZecoToolKit/Classes/UIKit/UIButton+ARTExtension.swift
+++ ARTZecoToolKit/Classes/UIKit/UIButton+ARTExtension.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
+private var art_customValueKey: UInt8 = 0
+
 extension UIButton {
+    
+    /// 自定义值，用于保存额外的数据.
+    ///
+    /// - Note: 使用 `objc_setAssociatedObject` 和 `objc_getAssociatedObject` 函数来实现.
+    var art_customValue: Any? {
+        get {
+            return objc_getAssociatedObject(self, &art_customValueKey)
+        }
+        set {
+            objc_setAssociatedObject(self, &art_customValueKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
     
     /// 设置图像为指定的状态，将其缩放到指定的大小.
     ///
@@ -25,3 +39,4 @@ extension UIButton {
         setImage(scaledImage, for: state)
     }
 }
+
