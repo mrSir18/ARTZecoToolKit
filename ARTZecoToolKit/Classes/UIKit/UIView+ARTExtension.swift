fab1dd2ad5381356fc080ceef73f5086d85bda96
mrSir18 <>
2024-07-05 06:02:08 +0800

    增加UIView扩展

---
+++ ARTZecoToolKit/Classes/UIKit/UIView+ARTExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIView+ARTExtension.swift
+//  ARTZecoToolKit
+//
+//  Created by mrSir18 on 2024/7/5.
+//
+
+extension UIView {
+    
+    /// 将视图转换为图片对象.
+    ///
+    /// - Returns: 生成的图片对象.
+    func asImageAsync(completion: @escaping (UIImage?) -> Void) {
+        DispatchQueue.global().async {
+            UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
+            defer { UIGraphicsEndImageContext() }
+            
+            guard let context = UIGraphicsGetCurrentContext() else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            self.layer.render(in: context)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+}
