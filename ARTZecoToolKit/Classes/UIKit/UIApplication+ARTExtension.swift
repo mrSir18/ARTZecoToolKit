ea6e059747dd6dd4fdc1b4f75685532a6a0c3f01
mrSir18 <>
2024-07-05 10:19:24 +0800

    添加UIApplication扩展协议

---
+++ ARTZecoToolKit/Classes/UIKit/UIApplication+ARTExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIApplication+ARTExtension.swift
+//  ARTZecoToolKit
+//
+//  Created by mrSir18 on 2024/7/5.
+//
+
+extension UIApplication {
+    
+    /// 返回当前应用程序的顶级视图控制器对象.
+    ///
+    /// - Parameter base: 要查找的视图控制器对象.
+    /// - Returns: 顶级视图控制器对象.
+    /// - Note: 该方法可能会返回 nil 值.
+    public class func topViewController(base: UIViewController? = UIApplication.shared.windows.first(where: \.isKeyWindow)?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
