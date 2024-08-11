ccf7e9ac390f601ac4f1606565f1c75a540919c5
mrSir18 <>
2024-08-11 22:57:14 +0800

    date调整

--- ARTZecoToolKit/Classes/UIKit/Date+ARTExtension.swift
+++ ARTZecoToolKit/Classes/UIKit/Date+ARTExtension.swift
@@ -10,7 +10,7 @@ extension Date {
     /// 获取格式化后的日期字符串
     /// - Parameter date: 需要格式化的日期对象
     /// - Returns: 格式化后的日期字符串，使用 "yyyy-MM-dd" 格式
-    public static func formattedDate(from date: Date) -> String {
+    public static func formattedDate(_ date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter.string(from: date)
@@ -19,10 +19,10 @@ extension Date {
     /// 将日期字符串转换为 Date 对象
     /// - Parameter dateString: 需要转换的日期字符串
     /// - Returns: 转换后的 Date 对象，如果转换失败则返回 nil
-    public static func dateFromString(_ dateString: String) -> Date? {
+    public static func dateFromString(_ dateString: String) -> Date {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"  // 内部定义的日期格式
-        return formatter.date(from: dateString)
+        return formatter.date(from: dateString) ?? Date()
     }
     
     /// 将时间戳转换为 Date 对象
