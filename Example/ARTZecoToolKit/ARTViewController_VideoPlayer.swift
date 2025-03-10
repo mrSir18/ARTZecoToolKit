656010b35abc980938f715fc7332944b66379ecc
mrSir18 <mrSir18@163.com>
2025-03-10 15:59:48 +0800

    1：播放器示例调整.

--- Example/ARTZecoToolKit/ARTViewController_VideoPlayer.swift
+++ Example/ARTZecoToolKit/ARTViewController_VideoPlayer.swift
@@ -158,7 +158,7 @@ import ARTZecoToolKit
 class ARTViewController_VideoPlayer: ARTBaseViewController {
     
     /// 播放器视图
-    public var playerView: ARTVideoPlayerView!
+    public var playerView: ARTVideoPlayerEnhancedWrapperView!
 
     
     // MARK: - Initialization
@@ -173,7 +173,7 @@ class ARTViewController_VideoPlayer: ARTBaseViewController {
 
     private func setupPlayerView() { // 创建播放器视图
         let aspectRatio: CGFloat = 16.0 / 9.0
-        playerView = ARTVideoPlayerView(self)
+        playerView = ARTVideoPlayerEnhancedWrapperView(self)
         view.addSubview(playerView)
         playerView.snp.makeConstraints { make in
             make.left.right.equalToSuperview()
